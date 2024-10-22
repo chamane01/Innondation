371fb5c9822b9089a6cd1638b7125629c6e5f32f
chamane01 <148882260+chamane01@users.noreply.github.com>
2024-10-22 18:01:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import folium
 from streamlit_folium import st_folium
 
 # Streamlit - Titre de l'application avec logo
-st.image("mon_logo.png", width=150)
+st.image("POPOPO.jpg", width=150)
 st.title("Carte des zones inondées avec niveaux d'eau et surface")
 
 # Étape 1 : Téléverser le fichier Excel ou TXT
