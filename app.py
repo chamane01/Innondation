74337fcab3e679257a494f2c6860c452634ef35a
chamane01 <148882260+chamane01@users.noreply.github.com>
2024-10-24 18:15:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -132,8 +132,9 @@ if df is not None:
             contours_inondation = ax.contour(grid_X, grid_Y, grid_Z, levels=[st.session_state.flood_data['niveau_inondation']], colors='red', linewidths=1)
             ax.clabel(contours_inondation, inline=True, fontsize=10, fmt='%1.1f m')
             # Tracé des hachures pour la zone inondée
-            ax_map.contourf(grid_X, grid_Y, grid_Z, levels=[-np.inf, niveau_inondation], colors='#007FFF', alpha=0)
-
+           ax.contourf(grid_X, grid_Y, grid_Z, 
+            levels=[-np.inf, st.session_state.flood_data['niveau_inondation']], 
+            colors='#007FFF', alpha=0.5)  # Couleur bleu semi-transparent
 
             # Tracer la zone inondée
           #  if polygon_inonde:
