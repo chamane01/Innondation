4fa14544dafdd32b9f39e21788ddb996414936c3
chamane01 <148882260+chamane01@users.noreply.github.com>
2024-11-30 18:31:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -273,6 +273,7 @@ def generate_depth_map(label_rotation_x=0, label_rotation_y=0):
         inline=True,
         fmt={seuil_bas_fond: f" {seuil_bas_fond:.2f} m"},  # Format du label
         fontsize=10
+        path_effects=[pe.withStroke(linewidth=3, foreground='white')]
     )
 
 
