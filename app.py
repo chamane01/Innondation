c296313171110d47c1482b3d861f6638f2a5435e
chamane01 <148882260+chamane01@users.noreply.github.com>
2024-12-01 22:30:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from shapely.geometry import MultiPolygon
 import contextily as ctx
 import ezdxf  # Bibliothèque pour créer des fichiers DXF
 from datetime import datetime
-import matplotlib.patches as mpatches
 
 # Streamlit - Titre de l'application avec deux logos centrés
 col1, col2, col3 = st.columns([1, 1, 1])
