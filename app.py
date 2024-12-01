af151fdc8b432ead13ef9dc7fa8bea06d00d9b11
chamane01 <148882260+chamane01@users.noreply.github.com>
2024-12-01 23:38:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -452,7 +452,16 @@ def generate_depth_map(ax, grid_Z, grid_X, grid_Y, X_min, X_max, Y_min, Y_max, l
             ax.plot(x, y, 'k+', markersize=7, alpha=1.0)
 
     # Ajouter des labels pour les contours
-    ax.clabel(contour_lines, inline=True, fmt={seuil_bas_fond: f"{seuil_bas_fond:.2f} m"}, fontsize=12, colors='white', fontweight='bold', path_effects=[plt.patheffects.withStroke(linewidth=2, foreground='black')])
+    ax.annotate(
+        label, 
+        (x, y), 
+        fontsize=12, 
+        ha="center", 
+        va="center", 
+        color="white",  # Couleur du texte
+        weight='bold',  # Gras
+        bbox=dict(facecolor='black', edgecolor='none', boxstyle="round,pad=0.5", alpha=0.6)  # Boîte noire autour du texte
+    )
 
     # Ajouter des lignes pour relier les tirets
     for x in np.linspace(X_min, X_max, num=5):
