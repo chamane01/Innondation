e1eab29fb0e936d256089510bfbcc4bb1eb522ae
chamane01 <148882260+chamane01@users.noreply.github.com>
2024-12-02 01:08:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -520,7 +520,7 @@ def generate_depth_map(ax, grid_Z, grid_X, grid_Y, X_min, X_max, Y_min, Y_max, l
     )
     ax.text(
         X_min + (X_max - X_min) * 0.22,  # Position horizontale (10% de la largeur)
-        label_y_position - (Y_max - Y_min) * 0.20,  # Légèrement plus bas
+        label_y_position - (Y_max - Y_min) * 0.24,  # Légèrement plus bas
         f"{seuil_bas_fond:.2f} m",
         fontsize=12,
         color="black",
