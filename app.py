58fbb4824994c48b52e08b2a6fb0ba6e2e444acb
chamane01 <148882260+chamane01@users.noreply.github.com>
2024-12-02 01:01:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -490,7 +490,7 @@ def generate_depth_map(ax, grid_Z, grid_X, grid_Y, X_min, X_max, Y_min, Y_max, l
     )
     ax.text(
         X_min + (X_max - X_min) * 0,  # Position horizontale (10% de la largeur)
-        label_y_position - (Y_max - Y_min) * 0.20,  # Légèrement plus bas
+        label_y_position - (Y_max - Y_min) * 0.10,  # Légèrement plus bas
         f"Cote du bafond :",
         fontsize=12,
         color="black",
@@ -500,7 +500,7 @@ def generate_depth_map(ax, grid_Z, grid_X, grid_Y, X_min, X_max, Y_min, Y_max, l
     )
     ax.text(
         X_min + (X_max - X_min) * 0,  # Position horizontale (10% de la largeur)
-        label_y_position - (Y_max - Y_min) * 0.20,  # Légèrement plus bas
+        label_y_position - (Y_max - Y_min) * 0.10,  # Légèrement plus bas
         f"Surface des bas-fonds dans les polygones :",
         fontsize=12,
         color="black",
