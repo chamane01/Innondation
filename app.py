d6f0bb528d8223b6d8ba2ae6cf1dff792f647682
chamane01 <148882260+chamane01@users.noreply.github.com>
2024-12-02 05:21:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -541,7 +541,7 @@ def generate_depth_map(ax, grid_Z, grid_X, grid_Y, X_min, X_max, Y_min, Y_max, l
                 # Retourner le fichier GPX sous forme de chaîne binaire
             return io.BytesIO(gpx.to_xml().encode())
 
-        def generate_shapefile(grid_X, grid_Y, grid_Z):
+    def generate_shapefile(grid_X, grid_Y, grid_Z):
             """Générer un fichier Shapefile à partir des données de grille"""
             gdf = gpd.GeoDataFrame(
             {'X': grid_X.flatten(), 'Y': grid_Y.flatten(), 'Z': grid_Z.flatten()},
@@ -552,7 +552,7 @@ def generate_depth_map(ax, grid_Z, grid_X, grid_Y, X_min, X_max, Y_min, Y_max, l
             shp_bytes.seek(0)
             return shp_bytes
 
-        def generate_geojson(grid_X, grid_Y, grid_Z):
+     def generate_geojson(grid_X, grid_Y, grid_Z):
             """Générer un fichier GeoJSON à partir des données de grille"""
             gdf = gpd.GeoDataFrame(
                 {'X': grid_X.flatten(), 'Y': grid_Y.flatten(), 'Z': grid_Z.flatten()},
