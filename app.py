959938c8baaebf1c9e54c65aad0909a4d5835d5c
chamane01 <148882260+chamane01@users.noreply.github.com>
2024-12-02 00:57:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -498,6 +498,17 @@ def generate_depth_map(ax, grid_Z, grid_X, grid_Y, X_min, X_max, Y_min, Y_max, l
         va="top",
         fontweight='bold',# Aligné en haut
     )
+    ax.text(
+        X_min + (X_max - X_min) * 0.3,  # Position horizontale (10% de la largeur)
+        label_y_position - (Y_max - Y_min) * 0.20,  # Légèrement plus bas
+        f"{seuil_bas_fond:.2f} m",
+        fontsize=12,
+        color="black",
+        ha="left",  # Aligné à gauche
+        va="top",
+        fontweight='bold',# Aligné en haut
+    )
+    
 
 
 # Ajouter les polygones sur la carte
