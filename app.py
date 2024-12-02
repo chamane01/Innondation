ad133f523b8c2c1a84873139459409265335dad0
chamane01 <148882260+chamane01@users.noreply.github.com>
2024-12-02 00:55:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -491,11 +491,12 @@ def generate_depth_map(ax, grid_Z, grid_X, grid_Y, X_min, X_max, Y_min, Y_max, l
     ax.text(
         X_min + (X_max - X_min) * 0,  # Position horizontale (10% de la largeur)
         label_y_position - (Y_max - Y_min) * 0.20,  # Légèrement plus bas
-        f"Cote du bafond :{seuil_bas_fond:.2f} m",
+        f"Cote du bafond :",
         fontsize=12,
         color="black",
         ha="left",  # Aligné à gauche
-        va="top",   # Aligné en haut
+        va="top",
+        fontweight='bold',# Aligné en haut
     )
 
 
