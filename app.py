f8224616a7871ca083e569d63d0b3a8e134418ec
chamane01 <148882260+chamane01@users.noreply.github.com>
2024-12-05 15:03:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -333,7 +333,22 @@ def charger_polygones(uploaded_file):
         polygones_dans_emprise = None
 
     return polygones_dans_emprise
+def charger_routes(uploaded_file):
+    try:
+        if uploaded_file is not None:
+            # Lire le fichier GeoJSON téléchargé pour les routes
+            routes_gdf = gpd.read_file(uploaded_file)
+            
+            # Convertir le GeoDataFrame des routes au CRS EPSG:32630
+            routes_gdf = routes_gdf.to_crs(epsg=32630)
+            
+        else:
+            routes_gdf = None
+    except Exception as e:
+        st.error(f"Erreur lors du chargement des routes : {e}")
+        routes_gdf = None
 
+    return routes_gdf
 # Fonction pour afficher les polygones
 def afficher_polygones(ax, gdf_polygones, edgecolor='white', linewidth=1.0):
     if gdf_polygones is not None and not gdf_polygones.empty:
@@ -341,11 +356,21 @@ def afficher_polygones(ax, gdf_polygones, edgecolor='white', linewidth=1.0):
     else:
         st.warning("Aucun polygone à afficher dans l'emprise.")
 
+def afficher_routes(ax, gdf_routes, color='blue', linewidth=2.0):
+    if gdf_routes is not None and not gdf_routes.empty:
+        gdf_routes.plot(ax=ax, color=color, linewidth=linewidth)
+    else:
+        st.warning("Aucune route à afficher.")
+
 # Exemple d'appel dans l'interface Streamlit
 st.title("Affichage des Polygones et Profondeur")
 
 # Téléchargement du fichier GeoJSON pour les polygones
 uploaded_file = st.file_uploader("Téléverser un fichier GeoJSON", type="geojson")
+# Téléchargement du fichier GeoJSON pour les routes
+uploaded_file_routes = st.file_uploader("Téléverser un fichier GeoJSON pour les routes", type="geojson")
+
+
 
 
 
