bcad7049e3067d4f5e58ae93722f742421a8c7fd
chamane01 <148882260+chamane01@users.noreply.github.com>
2024-12-09 09:58:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,30 +12,6 @@ import ezdxf  # Bibliothèque pour créer des fichiers DXF
 from datetime import datetime
 import rasterio
 
-# Streamlit - Titre de l'application avec deux logos centrés
-col1, col2, col3 = st.columns([1, 1, 1])
-with col1:
-    st.image("POPOPO.jpg", width=150)
-with col2:
-    st.image("logo.png", width=150)
-with col3:
-    st.write("")  # Cette colonne est laissée vide pour centrer les logos
-
-
-
-# Importer les bibliothèques nécessaires
-import streamlit as st
-import pandas as pd
-import numpy as np
-import geopandas as gpd
-import matplotlib.pyplot as plt
-from scipy.interpolate import griddata
-from shapely.geometry import Polygon, box
-from shapely.geometry import MultiPolygon
-import contextily as ctx
-import ezdxf  # Bibliothèque pour créer des fichiers DXF
-from datetime import datetime
-import rasterio  # Pour gérer les fichiers GeoTIFF
 
 # Streamlit - Titre de l'application avec deux logos centrés
 col1, col2, col3 = st.columns([1, 1, 1])
