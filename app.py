dffc16f55ae50784d3f1a828054c515e5e179618
chamane01 <148882260+chamane01@users.noreply.github.com>
2024-12-11 02:27:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def creer_carte_osm(data_tiff, bounds_tiff, niveau_inondation=None, **geojson_la
         "routes": {"color": "orange", "weight": 2},
         "polygon": {"fillColor": "transparent", "color": "black", "weight": 2},
         "pistes": {"color": "brown", "weight": 2},
-        "cours_eau": {"color": "cyan", "weight": 2},
+        "cours_eau": {"color": "blue", "weight": 2},
         "batiments": {"fillColor": "red", "color": "red", "weight": 1, "fillOpacity": 0.5},
         "ville": {"fillColor": "green", "color": "green", "weight": 1, "fillOpacity": 0.3},
         "plantations": {"fillColor": "yellow", "color": "yellow", "weight": 1, "fillOpacity": 0.3},
