b85f3b9bcea9847d358f755b30acd9cd5392e44e
chamane01 <148882260+chamane01@users.noreply.github.com>
2024-12-24 01:04:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -452,7 +452,7 @@ def generate_depth_map(ax, grid_Z, grid_X, grid_Y, X_min, X_max, Y_min, Y_max, l
             ax.plot(x, y, 'k+', markersize=7, alpha=1.0)
 
     # Ajouter des labels pour les contours
-    ax.clabel(contour_lines, inline=True, fmt={seuil_bas_fond: f"{seuil_bas_fond:.2f} m"}, fontsize=12)
+    ax.clabel(contour_lines, inline=True, fmt={seuil_bas_fond: f"{seuil_bas_fond:.2f} m"}, fontsize=12, colors='white')
 
     # Ajouter des lignes pour relier les tirets
     for x in np.linspace(X_min, X_max, num=5):
@@ -482,6 +482,15 @@ def generate_depth_map(ax, grid_Z, grid_X, grid_Y, X_min, X_max, Y_min, Y_max, l
         X_min + (X_max - X_min) * 0,  # Position horizontale (10% de la largeur)
         label_y_position - (Y_max - Y_min) * 0.10,  # Légèrement plus bas
         f"Surface des bas-fonds dans les polygones : {surface_bas_fond_polygones:.2f} hectares",
+        fontsize=12,
+        color="black",
+        ha="left",  # Aligné à gauche
+        va="top",   # Aligné en haut
+    )
+    ax.text(
+        X_min + (X_max - X_min) * 0,  # Position horizontale (10% de la largeur)
+        label_y_position - (Y_max - Y_min) * 0.20,  # Légèrement plus bas
+        f"Cote du bafond :{seuil_bas_fond:.2f} m",
         fontsize=12,
         color="black",
         ha="left",  # Aligné à gauche
