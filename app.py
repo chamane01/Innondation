11b336bd1289b7455f97ead9118e58d017903395
chamane01 <148882260+chamane01@users.noreply.github.com>
2024-12-27 21:31:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -53,10 +53,10 @@ def add_image_overlay(map_object, tiff_path, bounds, name):
 def main():
     st.title("TIFF Viewer and Interactive Map")
 
-    # Button to trigger the drawing functionality
-    if st.button('Dessin'):
-        # Upload TIFF file
-        uploaded_file = st.file_uploader("Upload a TIFF file", type=["tif", "tiff"])
+    # Button to toggle sidebar visibility
+    if st.button("Dessiner"):
+        with st.sidebar:
+            uploaded_file = st.file_uploader("Upload a TIFF file", type=["tif", "tiff"])
 
         if uploaded_file is not None:
             tiff_path = uploaded_file.name
